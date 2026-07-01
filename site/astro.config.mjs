3023b6d56d787cef08d95e0dbe6fb1e2bba1582b
Marcus <marcusianl@gmail.com>
2026-07-01 17:41:35 +0200

    Add basic website for hosting ToS and privacy policy

---
+++ site/astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// Deployed as a GitHub Pages project site at:
+// https://marcusianlevine.github.io/birds-bugs-botanicals/
+export default defineConfig({
+  site: 'https://bbb.marcuslevine.com',
+});
