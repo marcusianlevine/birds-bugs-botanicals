29b10b59e3c936ef9cee9727edaed5655ef93bb0
Marcus <marcusianl@gmail.com>
2026-07-03 10:29:48 +0200

    Expand site

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // Deployed as a GitHub Pages project site at:
 // https://marcusianlevine.github.io/birds-bugs-botanicals/
 export default defineConfig({
-  site: 'https://bbb.marcuslevine.com',
+  site: 'https://birdsbugsbotanicals.marcuslevine.com',
 });
